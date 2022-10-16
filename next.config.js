e8f9ac106c18729cf627e7270f34a603cd3bdcea
nkitlabs <natthakunk0109@gmail.com>
2022-10-16 10:38:27 +0700

    fix config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,46 @@
+const securityHeaders = [
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'deny',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'same-origin',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=()',
+  },
+]
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  distDir: 'out',
+  images: {
+    domains: ['storage.googleapis.com'],
+  }, //next-images
+  env: {
+    APP_ENV: process.env.APP_ENV,
+  },
+  async headers() {
+    return [
+      {
+        // * Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
